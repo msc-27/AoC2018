4efffb87bd51aa28d54e54fc6743fc3250b9100c
Mark <mark@msc27.me.uk>
2018-12-03 10:37:55 +0000

    day2 tidying

--- day2/day2.py
+++ day2/day2.py
@@ -10,13 +10,12 @@ for s in a:
 print(count2 * count3)
 
 a.sort()
-i = 0
+i = -1
 c = 0
 while c != 1:
+	i += 1
 	d = [x for x in range(len(a[i])) if a[i][x] != a[i+1][x]]
 	c = len(d)
-	i += 1
-i -= 1
 print(a[i])
 print(a[i+1])
-print(a[i][0:d[0]],a[i][d[0]+1:len(a[i])])
+print(a[i][:d[0]],a[i][d[0]+1:])
