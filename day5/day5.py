33cdda770643a3f60112925b47ee0f4646262d94
Mark <mark@msc27.me.uk>
2018-12-05 20:45:32 +0000

    day5

--- day5/day5.py
+++ day5/day5.py
@@ -2,12 +2,11 @@ a = open('day5').read().strip()
 d = {}
 for r in set(a.upper()+'#'): # char to remove (or dummy '#' for part 1)
     s = ''
-    for c in a:
-        if c.upper() != r:
-            if (s) and s[-1] != c and s[-1].upper() == c.upper():
-                s = s[:-1]
-            else:
-                s += c
+    for c in (x for x in a if x.upper() != r):
+        if (s) and s[-1].upper() == c.upper() and s[-1] != c:
+            s = s[:-1]
+        else:
+            s += c
     d[r] = len(s)
 print(d['#'])
 print(min(d.values()))
